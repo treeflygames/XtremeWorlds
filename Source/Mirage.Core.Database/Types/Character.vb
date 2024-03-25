877c2ef61adef960baf43e29540dc9b4a5c85cac
XerShade <xershade.ca@gmail.com>
2024-03-25 18:53:01 -0400

    Added property constructors.

--- Source/Mirage.Core.Database/Types/Character.vb
+++ Source/Mirage.Core.Database/Types/Character.vb
@@ -4,5 +4,9 @@ Namespace Types
     <PrimaryKey(NameOf(Character.Name))>
     Public Class Character
         Public Property Name As String
+
+        Public Sub New()
+            Me.Name = String.Empty
+        End Sub
     End Class
 End Namespace
