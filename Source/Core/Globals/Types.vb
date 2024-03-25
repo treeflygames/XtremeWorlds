e98b63fd4e3651161f54e6b25c3797e7ff8c0e5f
XerShade <xershade.ca@gmail.com>
2024-03-25 18:52:44 -0400

    Removed Charlist reference.

--- Source/Core/Globals/Types.vb
+++ Source/Core/Globals/Types.vb
@@ -25,7 +25,6 @@ Public Module Types
     Public TradeTheirOffer(MAX_INV) As PlayerInvStruct
     Public Party As PartyStruct
     Public MapResource() As MapResourceStruct
-    Public Chars As CharList
     Public Settings As Settings 
 
     Public Structure ResourceTypeStruct
